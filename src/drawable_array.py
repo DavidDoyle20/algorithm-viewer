424de3f340bc99ca8b2a9bb674396b79b2f4634d
DavidDoyle20 <davestar80@gmail.com>
2024-09-19 09:12:18 -0500

    added quicksort and mergesort

--- src/drawable_array.py
+++ src/drawable_array.py
@@ -11,6 +11,7 @@ class Drawable_Array:
 
     # Max not inclusive
     def generate_array(self, size=20, min_num=0, max_num=20, duplicates=True):
+        self.array = []
         if duplicates:
             for i in range(size):
                 self.array.append(random.randrange(min_num, max_num))
@@ -25,9 +26,9 @@ class Drawable_Array:
         print("Generated: ", self.array)
         self.max_val = max(self.array)
     
-    def draw_array(self, display_numbers=False):
+    def draw_array(self, color="cyan", display_numbers=False):
         for i in range(len(self.array)):
-            self.draw_indexes(i, color="cyan", display_numbers=display_numbers)
+            self.draw_indexes(i, color=color, display_numbers=display_numbers)
 
     def selection_sort(self):
         if not self.array: return None
@@ -36,8 +37,10 @@ class Drawable_Array:
             self.draw_indexes(x, color="red")
             min_index = x
             for j in range(x+1, len(self.array)):
+                self.draw_indexes(j, color="red")
                 if self.array[j] < self.array[min_index]:
                     min_index = j
+                self.draw_indexes(j, color="cyan", display_numbers=True)
             temp = self.array[x]
             self.array[x] = self.array[min_index]
             self.draw_indexes(x, color="SpringGreen2", display_numbers=True)
@@ -84,9 +87,87 @@ class Drawable_Array:
             self.array[j + 1] = key # The position where the key needs to be inserted was found
             self.draw_indexes(j + 1, color="SpringGreen2", display_numbers=True)
 
-        
+    def merge_sort(self, left, right):
+        def merge(left, mid, right):
+            n1 = mid - left + 1
+            n2 = right - mid
             
-        print(self.array)
+            L = [0] * n1
+            R = [0] * n2
+
+            for i in range(n1):
+                L[i] = self.array[left + i]
+            for j in range(n2):
+                R[j] = self.array[mid + 1 + j]
+            
+            i = 0
+            j = 0
+            k = left
+
+            while i < n1 and j < n2:
+                if L[i] <= R[j]:
+                    self.array[k] = L[i]
+                    self.draw_indexes(k, color="SpringGreen2", display_numbers=True)
+                    i += 1
+                else:
+                    self.array[k] = R[j]
+                    self.draw_indexes(k, color="SpringGreen2", display_numbers=True)
+                    j += 1
+                k += 1
+
+            while i < n1:
+                self.array[k] = L[i]
+                self.draw_indexes(k, color="SpringGreen2", display_numbers=True)
+
+                i += 1
+                k += 1
+
+            while j < n2:
+                self.array[k] = R[j]
+                self.draw_indexes(k, color="SpringGreen2", display_numbers=True)
+
+                j += 1
+                k += 1
+                
+        if left < right:
+            mid = (left + right) // 2
+            self.merge_sort(left, mid)
+            self.merge_sort(mid + 1, right)
+            merge(left, mid, right)
+
+
+    # The QuickSort function implementation
+    def quick_sort(self, low, high):
+        def partition(low, high):
+            # Choose the pivot
+            pivot = self.array[high]
+            self.draw_indexes(low, high, color="red", display_numbers=True)
+            
+            i = low - 1
+            
+            # Traverse arr[low..high] and move all smaller
+            # elements on the left side. Elements from low to 
+            # i are smaller after every iteration
+            for j in range(low, high):
+                if self.array[j] < pivot:
+                    i += 1
+                    self.array[i], self.array[j] = self.array[j], self.array[i]
+                    self.draw_indexes(i, j, color="SpringGreen2", display_numbers=True)
+                self.draw_indexes(i, j, color="cyan", display_numbers=True)
+            
+            # Move pivot after smaller elements and
+            # return its position
+            self.array[i + 1], self.array[high] = self.array[high], self.array[i + 1]
+            self.draw_indexes(i+1, high, color="SpringGreen2", display_numbers=True)
+            return i + 1
+        if low < high:
+            # pi is the partition return index of pivot
+            pi = partition(low, high)
+
+            # Recursion calls for smaller elements
+            # and greater or equals elements
+            self.quick_sort(low, pi - 1)
+            self.quick_sort(pi + 1, high)
             
     def draw_indexes(self, *args, color="orange", display_numbers=False):
         width = self.win.width
@@ -112,6 +193,6 @@ class Drawable_Array:
     
     def animate(self):
         self.win.redraw()
-        time.sleep(0.1)
+        time.sleep(0.05)
 
 
--- src/main.py
+++ src/main.py
@@ -2,16 +2,26 @@ from window import Window
 from drawable_array import Drawable_Array
 
 def main():
-    win = Window(500, 500)
+    win = Window(1000, 1000)
 
     dr = Drawable_Array(win=win)
-    dr.generate_array(size=50,min_num=1, max_num=51, duplicates=False)
-
+    dr.generate_array(size=200,min_num=1, max_num=201, duplicates=False)
     dr.draw_array(display_numbers=True)
-    #dr.selection_sort()
-    #dr.bubble_sort()
+    """
+    dr.selection_sort()
+    dr.generate_array(size=50,min_num=1, max_num=51, duplicates=False)
+    dr.draw_array(display_numbers=True)
+    dr.bubble_sort()
+    dr.generate_array(size=50,min_num=1, max_num=51, duplicates=False)
+    dr.draw_array(display_numbers=True)
     dr.insertion_sort()
-
+    dr.generate_array(size=50,min_num=1, max_num=51, duplicates=False)
+    dr.draw_array(display_numbers=True)
+    """
+    dr.merge_sort(0, len(dr.array)-1)
+    """
+    dr.quick_sort(0, len(dr.array)-1)
+    """
     win.wait_for_close()
     
 main()
